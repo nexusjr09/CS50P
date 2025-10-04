7af01857dccf5e23059a4fdde1506d8f20fe0590
Vigyan_07 <baralbigyan48@gmail.com>
2025-10-04 12:06:27 +0545

    lecture 0 mid

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-name = input("Enter your name : ").strip().capitalize()
+name = input("Enter your name : ").strip().capitalize() #strip only removes the spaces from left and right but not in middle 
 print(name)

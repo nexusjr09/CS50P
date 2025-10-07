fbd4a466c28e31dbaefda5f3abeb99d2d616a4d8
Vigyan_07 <baralbigyan48@gmail.com>
2025-10-07 18:15:13 +0545

    updates

--- 1prac.py
+++ 1prac.py
@@ -1,7 +1,3 @@
 input1 = int(input("Enter the first Number : "))
 input2 = int(input("Enter the 2nd number : "))
 print(input1 * input2)
-
-input1 = int(input("Enter the first Number : "))
-input2 = int(input("Enter the 2nd number : "))
-print(input1 * input2)

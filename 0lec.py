21a90646fb00ebbce6dcfcebaa828c0b31425aff
Vigyan_07 <baralbigyan48@gmail.com>
2025-10-07 18:14:54 +0545

    changes

--- 0lec.py
+++ 0lec.py
@@ -2,3 +2,4 @@ name = input("Enter your name : ").strip().title() #strip only removes the space
 #name.lstrip()--> will remove spaces only from left and #name.rstrip()--> will remove spaces only from right
 #name.capitalize()--> will only capitalize the first word not others . #name.title() --> will capitalize the first letter of each word ! 
 print(name)
+
---
+++ 1prac.py
@@ -0,0 +1,7 @@
+input1 = int(input("Enter the first Number : "))
+input2 = int(input("Enter the 2nd number : "))
+print(input1 * input2)
+
+input1 = int(input("Enter the first Number : "))
+input2 = int(input("Enter the 2nd number : "))
+print(input1 * input2)

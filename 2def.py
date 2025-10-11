a364f99b3fe113c308ebdf8799fb9187d6b12909
Vigyan_07 <baralbigyan48@gmail.com>
2025-10-11 08:43:43 +0545

    changes

--- 2def.py
+++ 2def.py
@@ -1,4 +1,10 @@
-def hello (x):
+def hello (x = "Bro"): #here x is the Parameter #hello is the function #"Bro"   is the argument ! 
     print("hello , " , x)
+hello()
 name = input("Enter your name : ")
 hello(name)
+
+
+def main(a):
+    name =     print("Enter your name : ")
+main()

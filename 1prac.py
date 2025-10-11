9e41ad181f35f9af370f204b869a8d1924c8888d
Vigyan_07 <baralbigyan48@gmail.com>
2025-10-11 07:59:33 +0545

    changes :

--- 1prac.py
+++ 1prac.py
@@ -3,7 +3,7 @@
 x =  float(input('ENter your 1st num  : '))
 y = float(input("Enter your 2nd num : "))
 z = round(x + y )
-print(z)
 
 #adding  :, in the  number will format is automatic : 
 print(f"{z:,}")
+print(f"{z:.2f}") #for decimal to be only of two digits 

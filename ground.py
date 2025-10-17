474cc87bd4149a28d739401d1abbf2d9a7ea5aaa
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-10-17 15:35:17 +0545

    changes

--- ground.py
+++ ground.py
@@ -1,7 +1,7 @@
-datas = [
-     {"Name":"Bigyan","House":"Fikkal","Skin":"White"},
-     {"Name":"Suresh","House":"Palpa","Skin":"Black"},
-     {"Name":"Dipesh","House":"Jhapa","Skin":"Brown" },
-] 
-for data in datas:
-    print(data["Name"],data["House"],data["Skin"])
+def main():
+    hastags(5)
+
+
+def hastags(width):
+    print("##### \n" * width )
+main()

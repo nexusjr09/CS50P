0aa7f5560ebb9f5292aafbcf01de1cc9a35e2fcf
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-10-19 21:23:06 +0545

    vanity

--- ground.py
+++ ground.py
@@ -1,9 +1,19 @@
-texts = input("Enter your string : ").lower()
-toremove = ["a","e","i","o","u"]
-clean = ""
-for i in range(len(texts)):
-    if i in toremove:
-        clean = clean + texts.replace(i,"")
+#start with 2 letters
+#minimum 2 words maximum 6 (could be all letters + numbers at the end 
+#numbers should only be at last 
+#first number cannot be zero 
+#no special characters 
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
     else:
-        texts = clean
-print(clean)
+        print("Invalid")
+
+
+def is_valid(s):
+   start_two = s[:2] 
+   if len(s)>= 2 and len(s)<=6 and start_two.isalpha() and s.isalnum():    #isalnum()forspecialcharacter
+       return s
+main()

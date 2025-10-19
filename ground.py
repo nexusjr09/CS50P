93497c581fe368d6624704e3af36359f46c64936
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-10-19 12:33:54 +0545

    change

--- ground.py
+++ ground.py
@@ -1,7 +1,11 @@
-def main():
-    hastags(5)
-
-
-def hastags(width):
-    print("##### \n" * width )
-main()
+amount = 0 
+coins = [5 , 10 , 25]
+while amount < 50:
+    print(f"Amount Due : {50-amount}")
+    coin = int(input("Enter coin: "))
+    if coin in coins:
+        amount = amount + coin
+    else:
+        print("Enter coin in this denominations : 5 or 10 or 25 ")
+else:
+    print(f"Change Owed: {amount - 50} ")

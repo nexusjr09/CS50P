96180cae885767a2c57c8b7a895b31436a2cb017
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-10-29 17:33:34 +0545

    changes

--- CS50P/ground.py
+++ CS50P/ground.py
@@ -1,6 +1,7 @@
 import cowsay
 import sys
 import requests
+import json
 
-data = requests.get("https://github/" + sys.argv[1])
-print(data.json)
+data = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+print(json.dumps(data.json(), indent=2))

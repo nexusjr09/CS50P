ae5e14d3d312e8ff8ecd0151206bc32bfce15e8c
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-10-29 17:52:48 +0545

    change

--- CS50P/ground.py
+++ CS50P/ground.py
@@ -3,5 +3,9 @@ import sys
 import requests
 import json
 
-data = requests.get("https://itunes.apple.com/search?entity=song&limit=5&term=" + sys.argv[1])
-print(json.dumps(data.json(), indent=2))
+data = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+
+
+raw = data.json()
+for r in raw["results"]:
+    print(r["trackName"])

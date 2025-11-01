b9eca6d3ff5f8429c427dd3c5bed2b80b1f8b39d
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-11-01 08:35:32 +0545

    changes

--- CS50P/deep.py
+++ CS50P/deep.py
@@ -1,8 +1,9 @@
-answer = input("What is the answer to the Great Question of Life , the Universe, and Everything? : ")
+answer = input(
+    "What is the answer to the Great Question of Life , the Universe, and Everything? : "
+)
 answer = answer.lower().strip()
 match answer:
-    case "42" | "forty two" | "forty-two" :
+    case "42" | "forty two" | "forty-two":
         print("Yes")
     case _:
         print("No")
-

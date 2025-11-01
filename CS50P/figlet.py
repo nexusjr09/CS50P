18ce3092b25536e62417b2ded3b961332f6b8d33
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-11-01 17:50:16 +0545

    figlet

--- CS50P/figlet.py
+++ CS50P/figlet.py
@@ -10,3 +10,13 @@ if len(sys.argv) == 1:
     figlet.setFont(font= choose)
     data = input("Input: ")
     print(figlet.renderText(data))
+elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
+    fontname = sys.argv[2]
+    if fontname not in figlet.getFonts():
+        sys.exit("Invalid FontName")
+    else:
+        figlet.setFont(font=fontname)
+        data = input("Input: ")
+        print(figlet.renderText(data))
+else:
+    sys.exit("Invalid Format")

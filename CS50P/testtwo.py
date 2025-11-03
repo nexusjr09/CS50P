3400e7d1a20ac56e27729f338af13e3b5a6ad00f
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-11-03 17:12:40 +0545

    changes

--- CS50P/testtwo.py
+++ CS50P/testtwo.py
@@ -4,10 +4,9 @@ def main():
     checking()
 
 def checking():
-    if calculation(2) != 4:
-        print("wrong !")
-    elif calculation(4) != 16:
-        print("Wrong its 16")
-
+    try:
+        assert calculation(2) == 4
+    except AssertionError:
+        print("Negative ! Not correct !")
 if __name__ == "__main__":
     main()

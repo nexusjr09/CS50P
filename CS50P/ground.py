9aec702d08842bfbaa50db8693cff281fd90645e
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-11-04 19:38:34 +0545

    ground.py

--- CS50P/ground.py
+++ CS50P/ground.py
@@ -1,9 +1,34 @@
-greet = input("Greeting : ")
-greet = greet.lower().strip()
-if greet.startswith("hello"):
-    print("$0")
-elif greet.startswith("h"):
-    print("$20")
-else:
-    print("$100")
-  
+def main():
+    while True:
+        try:
+            data = input("Fraction: ")
+            result = convert(data)
+            gauge(result)
+            break
+        except (ValueError, ZeroDivisionError):
+            continue
+
+def convert(fraction):
+    x_str, y_str = fraction.split("/")
+    x = int(x_str)
+    y = int(y_str)
+    if x > y or x < 0 or y <= 0:
+        raise ValueError
+    return round((x / y) * 100)
+
+def gauge(percentage):
+    if percentage <= 1:
+        print("E")
+    elif percentage >= 99:
+        print("F")
+    else:
+        print(f"{percentage}%")
+
+if __name__ == "__main__":
+    main()
+
+    
+
+
+if __name__ == "__main__":
+    main()

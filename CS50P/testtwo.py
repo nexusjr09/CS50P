7509435282b137c36c9045b9a20a93d31579443a
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-11-04 08:19:26 +0545

    assertion

--- CS50P/testtwo.py
+++ CS50P/testtwo.py
@@ -1,16 +1,6 @@
-from testone import calculation
-
 def main():
-    checking()
+    data = input("Enter the string: ")
+    valueof(data)
 
-def checking():
-   try:
-      assert calculation(2) == 4
-      assert calculation(3) == 9
-      assert calculation(4) == 16
-   except AssertionError:
-       print("Invalid logic")
-
-
-if __name__ == "__main__":
-    main()
+def valueof(to="bigyan"):
+    print("Hello,", to)

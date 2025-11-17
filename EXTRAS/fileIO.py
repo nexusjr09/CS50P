06aaeb659573c2e993b602d8e3a5a59c508e145d
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-11-17 13:25:15 +0545

    chnages :

--- EXTRAS/fileIO.py
+++ EXTRAS/fileIO.py
@@ -1,5 +1,6 @@
+students = []
 with open ("names.csv") as file:
-    for line in sorted(file):
+    for line in file:
         dat = line.strip().split(",")
         print(f"{dat[0]} is from {dat[1]}")
         

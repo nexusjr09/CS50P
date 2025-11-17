7d54fac7a3e6901c67e1ff7f81a1d24f05d1c0db
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-11-17 13:55:20 +0545

    changes

--- EXTRAS/fileIO.py
+++ EXTRAS/fileIO.py
@@ -1,6 +1,13 @@
 students = []
 with open ("names.csv") as file:
     for line in file:
-        dat = line.strip().split(",")
-        print(f"{dat[0]} is from {dat[1]}")
+        name,house = line.strip().split(",")
+        student = {"name": name , "house": house}
+        students.append(student) 
+
+def get_name(student):
+    return student["name"] 
+
+for student in sorted(students , key = get_name):
+    print(f"{name} is from {house}")      
         

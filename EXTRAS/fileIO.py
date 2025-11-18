fed9b27b97a5b64ede6b92e48655490191d20ddf
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-11-18 15:10:47 +0545

    changes

--- EXTRAS/fileIO.py
+++ EXTRAS/fileIO.py
@@ -7,3 +7,7 @@ with open ("names.csv") as file:
 
 def get_name(student):
     return student["house"] 
+
+for student in sorted(students , key = get_name):
+    print(f"{ student['name'] } is from {student['house']}")      
+        

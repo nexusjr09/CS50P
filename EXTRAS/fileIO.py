0b3a49e19138a58f09f9fa1b6a15e37fa3e01920
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-11-21 08:18:06 +0545

    changes

--- EXTRAS/fileIO.py
+++ EXTRAS/fileIO.py
@@ -4,9 +4,9 @@ with open ("names.csv") as file:
     reader = csv.reader(file)
     for name,home in  reader:
         students.append({"name": name, "home": home})
-        
+
 
 
 for student in sorted(students , key = lambda student: student["name"]):
-    print(f"{ student['name'] } is from {student['house']}")      
+    print(f"{ student['name'] } is from {student['home']}")      
         

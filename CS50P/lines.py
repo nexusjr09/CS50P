f3354c96276e0ad465e84b67c56b5d289a271b40
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-11-25 07:51:26 +0545

    counting the lines

--- CS50P/lines.py
+++ CS50P/lines.py
@@ -1,7 +1,16 @@
 import sys
 import os
-
-if len(sys.argv) == 2 and sys.argv[1].endswith(".py"):
-    print('ready')
-else:
-    print("error") 
+count = 0
+try:
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".py"):
+        for line in sys.argv[1]:
+            line = line.strip()
+            if line == "":
+                continue
+            if line.startswith("#"):
+                continue
+            else:
+                count += 1 
+except FileNotFoundError:
+    sys.exit()
+print(count)

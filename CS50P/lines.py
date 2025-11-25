71ba174552f237a33f74b0cf79386302fde8123c
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-11-25 08:16:08 +0545

    Counting Lines Problem Set : |

--- CS50P/lines.py
+++ CS50P/lines.py
@@ -1,16 +1,22 @@
 import sys
-import os
+
+if len(sys.argv) < 2:
+    sys.exit("Too few command-line arguments")
+if len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+
+file_path = sys.argv[1]
+if not file_path.endswith(".py"):
+    sys.exit("Not a Python file")
+
 count = 0
 try:
-    if len(sys.argv) == 2 and sys.argv[1].endswith(".py"):
-        for line in sys.argv[1]:
+    with open(file_path) as file:
+        for line in file:
             line = line.strip()
-            if line == "":
-                continue
-            if line.startswith("#"):
-                continue
-            else:
-                count += 1 
+            if line and not line.startswith("#"):
+                count += 1
 except FileNotFoundError:
-    sys.exit()
+    sys.exit("File does not exist")
+
 print(count)

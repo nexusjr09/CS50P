dbc48c36ef589d4a70272410ddd0bfa7f0efc36c
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-11-26 08:08:25 +0545

    changes

--- CS50P/scourgify.py
+++ CS50P/scourgify.py
@@ -7,10 +7,14 @@ if len(sys.argv) < 3:
     sys.exit("Too Few command-line arguments ")
 oldfile = sys.argv[1]
 newfile = sys.argv[2]
+
+
 if not oldfile.endswith(".csv") or not newfile.endswith(".csv"):
     sys.exit("Invalid FileName")
-with open (oldfile) as file:
-    file_list = csv.reader(file)
+
+try:
+    with open (oldfile) as file:
+        file_list = csv.reader(file)
     for line in file_list:
         name = line[0].strip("'")
         first_name , last_name = name.split(", ")   
@@ -18,3 +22,5 @@ with open (oldfile) as file:
         print(first_name)
         print(last_name)
         print(house)   
+except FileNotFoundError:
+    sys.exit("File doesnot exits !")

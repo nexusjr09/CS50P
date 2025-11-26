c6026285e9a9ad435f0b9e540b06a52639d7d604
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-11-26 15:17:16 +0545

    updates

---
+++ CS50P/shirt.py
@@ -0,0 +1,21 @@
+import sys
+
+if len(sys.argv) > 3:
+    sys.exit("Too many command-line arguments ")
+if len(sys.argv) < 3:
+    sys.exit("Too few command-line arguments")
+
+rawfile = sys.argv[1].lower()
+finalfile = sys.argv[2].lower()
+
+valid_ext = (".jpeg",".png",".jpg")
+
+if not rawfile.endswith(valid_ext):
+    sys.exit("Incorrect file format of Input file")
+if not finalfile.endswith(valid_ext):
+    sys.exit("Incorrect file format of Output File")
+in_ext = rawfile.split(".")[-1]
+out_ext = finalfile.split(".")[-1]
+if in_ext!=out_ext:
+    sys.exit("Both the input and output file  must have same extension !")
+

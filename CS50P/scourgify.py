432626aad18eea3e401e07435c4cb3881e1dc860
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-11-26 11:59:45 +0545

    first part

--- CS50P/scourgify.py
+++ CS50P/scourgify.py
@@ -9,3 +9,12 @@ oldfile = sys.argv[1]
 newfile = sys.argv[2]
 if not oldfile.endswith(".csv") or not newfile.endswith(".csv"):
     sys.exit("Invalid FileName")
+
+before = sys.argv[1]
+after = sys.argv[2]
+with open(before,"r") as file:
+    dict_format = csv.DictReader(file)
+    for student in dict_format:
+        last,first = student["name"].split(", ")
+        house = student["house"]
+        

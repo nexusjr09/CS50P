889cb21ca50891730f183f107cd82e6bde5acead
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-01 19:40:04 +0545

    changes

--- CS50P/ground.py
+++ CS50P/ground.py
@@ -2,7 +2,7 @@ import re
 
 email = input("enter the email address: ")
 
-if re.search(r"^[a-zA-Z]+@[A-Za-z]+\.edu$",email):
+if re.search(r"^[a-zA-Z]+@[A-Z0-9]+\.edu$",email):
     print("valid ")
 else:
     print("Invalid")

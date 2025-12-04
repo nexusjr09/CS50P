b689b74bd1542c323f60a978c1cbcc13cecb074e
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-04 19:53:36 +0545

    REGEX

--- CS50P/twitter.py
+++ CS50P/twitter.py
@@ -1,4 +1,5 @@
 import re 
 url = input("Enter your twitter profile Link: ").lower().strip()
-actual = re.search(r"^(https?://)?(www\.)?twitter\.com/(.+)$","",url,re.IGNORECASE)
-print(actual)
+actual = re.search(r"^(https?://)?(www\.)?twitter\.com/(.+)$",url,re.IGNORECASE)
+if actual:
+    print(f"Username: {actual.group(3)}")

e843efd0ac6ab1284152e8b3fb570004eed2fded
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-04 12:11:51 +0545

    formating a string

--- CS50P/format.py
+++ CS50P/format.py
@@ -1,7 +1,6 @@
 import re 
 
 name = input("Enter your full name: ").strip()
-matches = re.search(r"^(.+), ?(.+)$",name)
-if matches:
+if matches := re.search(r"^(.+), ?(.+)$",name): #:= walrus operator
     name = matches.group(2) + " " + matches.group(1)
 print(f"Hello {name}")

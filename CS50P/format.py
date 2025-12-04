d4541c3bb1173e4f9a17d51dff09c3923f9ff2df
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-04 11:42:29 +0545

    formating a string

--- CS50P/format.py
+++ CS50P/format.py
@@ -3,6 +3,7 @@ import re
 name = input("Enter your full name: ").strip()
 matches = re.search(r"^(.+), (.+)$",name)
 if matches:
-    last,first = matches.groups()
+    last = matches.group(1)
+    first = matches.group(2)
     name = f"{first} {last}"
 print(f"Hello {name}")

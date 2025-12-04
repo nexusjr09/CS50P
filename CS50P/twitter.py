6bc4f834ea428a1339fb46a8baa2af1cf7f6e88a
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-04 13:22:40 +0545

    TWITTER

--- CS50P/twitter.py
+++ CS50P/twitter.py
@@ -1,3 +1,4 @@
+import re 
 url = input("Enter your twitter profile Link: ").lower().strip()
-actual = url.removeprefix("https://twitter.com/","")
-print(f"Username: {actual}")
+actual = re.sub(r"htpps://twitter.com/","")
+print(actual)

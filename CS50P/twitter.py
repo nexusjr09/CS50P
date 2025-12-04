d8e83ad9c002275407c9c9e25b727b3e9f72a504
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-04 19:51:13 +0545

    REGEX

--- CS50P/twitter.py
+++ CS50P/twitter.py
@@ -1,4 +1,4 @@
 import re 
 url = input("Enter your twitter profile Link: ").lower().strip()
-actual = re.sub(r"(https?://)?(www\.)?twitter.com/","",url)
+actual = re.search(r"^(https?://)?(www\.)?twitter\.com/(.+)$","",url,re.IGNORECASE)
 print(actual)

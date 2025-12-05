0e3341964c62b7846a4a3ab3d888c2bc95c765ec
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-05 16:35:53 +0545

    changes

--- CS50P/regexnumber.py
+++ CS50P/regexnumber.py
@@ -2,8 +2,8 @@ import re
 
 data = {"+977":"Nepal", "+1":"USA","+62":"Indonesia"}
 number = input("Enter the number: ")
-pattern = r"\+\d{1,3} \d{3}-\d{3}-\d{4}"
+pattern = r"(\+\d{1,3}) \d{3}-\d{3}-\d{4}"
 if match := re.search(pattern,number):
-    print("Valid !!")
+    print(f"The Mobile number is from: {data[match.group(1)]} ")
 else:
     print("Invalid !!")

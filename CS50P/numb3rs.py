193ea3310d66179760e8b83f5498817b13606211
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-06 12:34:02 +0545

    REGEX

--- CS50P/numb3rs.py
+++ CS50P/numb3rs.py
@@ -1,25 +1,22 @@
-import re
-import sys
-
-
 def main():
-    ip_adress = input("Enter your ipv4 adress: ")
-    print(validate(ip_adress))
+    ip_address = input("Enter your IPv4 address: ")
+    print(validate(ip_address))
 
 
 def validate(ip):
     parts = ip.split(".")
-    #REGEX  PATTERN WILL  BE TOO COMPLEX SO WE USED THIS NORMAL METHOD WHICH WAS ALLOWED
     if len(parts) != 4:
         return False
 
     for p in parts:
         if not p.isdigit():
             return False
-        if not 0 < int(p) <= 255:
+        if len(p) > 1 and p[0] == "0":
             return False
-
+        if not 0 <= int(p) <= 255:
+            return False
     return True
+
 
 if __name__ == "__main__":
     main()

c3ab223af8bc2d918311f610383147ce568206d6
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-06 12:00:02 +0545

    REGEx

--- CS50P/numb3rs.py
+++ CS50P/numb3rs.py
@@ -3,14 +3,17 @@ import sys
 
 
 def main():
-    print(validate(input("IPv4 Address: ")))
+    ip_adress = input("Enter your ipv4 adress: ")
+    print(validate(ip_adress))
 
 
 def validate(ip):
-    ...
+    pattern = r"^\d{0,255}\.\d{0,255}\.\d{0,255}\.\d{0,255}$"
+    if match :=re.search(pattern,ip):
+        return("Valid")
+    else:
+        return("Invalid")
 
-
-...
 
 
 if __name__ == "__main__":

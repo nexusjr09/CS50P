f9e9002a2ca24b6b554d9be9f6c4f787e2e4ba81
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-08 11:12:26 +0545

    WATCH

--- CS50P/watch.py
+++ CS50P/watch.py
@@ -1,3 +1,8 @@
 import re 
 
 html_data = input("Enter the links: ")
+pattern = r'^src="(https?://(?:www\.)youtube\.com/embed/[a-zA-Z_-]+)"$'
+if match := re.search(pattern,html_data):
+    print(match.group(1))
+else:
+    print("Invalid")

0fcc623770ca8fdcdd4a607f56e9399488fdee29
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-08 11:20:22 +0545

    WATCH set

--- CS50P/watch.py
+++ CS50P/watch.py
@@ -1,8 +1,16 @@
-import re 
+import re
 
-html_data = input("Enter the links: ")
-pattern = r'^src="(https?://(?:www\.)youtube\.com/embed/[a-zA-Z_-]+)"$'
-if match := re.search(pattern,html_data):
-    print(match.group(1))
-else:
-    print("Invalid")
+def main():
+    data = input("Enter the html code: ")
+    print(parse(data))
+
+def parse(s):
+    pattern = r'src="(https?://(?:www\.)?youtube\.com/embed/[a-zA-Z0-9_-]+)"'
+    if match:= re.search(pattern,s):
+        return(match.group(1))
+    else:
+        return None
+    
+
+if __name__ == "__main__":
+    main()

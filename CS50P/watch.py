6ff802462381239298d70d8925cd316aa3002af4
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-08 13:44:33 +0545

    changes error

--- CS50P/watch.py
+++ CS50P/watch.py
@@ -1,16 +1,18 @@
 import re
 
 def main():
-    data = input("Enter the html code: ")
-    print(parse(data))
-
+    print(parse(input("Enter the html code:")))
 def parse(s):
-    pattern = r'src="(https?://(?:www\.)?youtube\.com/embed/[a-zA-Z0-9_-]+)"'
+    pattern = r'src="(https?://(?:www\.)?youtube\.com/embed/([a-zA-Z0-9_-]+))"'
     if match:= re.search(pattern,s):
-        return(match.group(1))
+        two = match.group(1)
+        return (f"https://youtu.be/{two}")
     else:
         return None
-    
+
 
 if __name__ == "__main__":
     main()
+
+#https://youtu.be/xvFZjo5PgG0
+#https://www.youtube.com/embed/xvFZjo5PgG0

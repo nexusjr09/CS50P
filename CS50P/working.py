c06bc30a77ec5bbdf783c24a4088ddeb6474be47
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-09 18:33:36 +0545

    changes

--- CS50P/working.py
+++ CS50P/working.py
@@ -1,3 +1,28 @@
+import re
+import sys
+
+
+def main():
+    print((convert(input("Hours: "))).lower())
+
+
+def convert(s):
+    pattern = r"(/d*)(:/d/d)? (/w/w) to (/d*)(:/d/d)? (/w/w)"
+    if match:= re.search(pattern,s):
+        start_hour = match.group(1)
+        start_minute = match.group(2)
+        start_meridian = match.group(3)
+        end_hour = match.group(4)
+        end_minute = match.group(5)
+        end_meridian = match.group(6)
+        
+
+
+
+
+
+if __name__ == "__main__":
+    main()
 
 #9:00 AM to 5:00 PM
 #9 AM to 5 PM

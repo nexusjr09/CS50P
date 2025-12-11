75bfdd32602dbd90768fe8117fce4a7ca0c47de7
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-11 08:41:40 +0545

    REGEX

--- CS50P/working.py
+++ CS50P/working.py
@@ -15,8 +15,10 @@ def convert(s):
         end_hour = match.group(4)
         end_minute = match.group(5)
         end_meridian = match.group(6)
-        
-
+        if start_meridian == "PM" and start_hour < 12:
+            start_hour = int(start_hour) + 12
+            if end_meridian == "PM" and end_hour < 12:
+                end_hour = int(end_hour) + 12
 
 
 

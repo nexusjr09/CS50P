5c70d232f7b36378d276db460a3dfeb1ab9d70db
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2025-12-18 20:09:55 +0545

    : changes

--- CS50P/twitter.py
+++
@@ -1,4 +0,0 @@
-import re 
-url = input("Enter your twitter profile Link: ").lower().strip()
-if actual := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)",url,re.IGNORECASE):
-    print(f"Username: {actual.group(1)}")

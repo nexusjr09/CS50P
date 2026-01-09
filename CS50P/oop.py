684bcb9938d611437d10bc66c3883f9bf1d703f6
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-01-09 08:41:23 +0545

    OOP in python

---
+++ CS50P/oop.py
@@ -0,0 +1,25 @@
+#OBJECT ORIENTED PROGRAMMING
+
+class Student:
+    def __init__(self,name,house):
+        if not name:
+            return ValueError("Missing Name")
+        if house not in ["Gryffindor","Hufflepuff","Ravenclaw","Slytherin"]:
+            raise ValueError("Invalid House")
+        self.name = name
+        self.house = house
+
+
+
+def main():
+    student = get_student()
+    print(f"{student.name} is name and {student.house} is house")
+
+
+def get_student():
+    name = input("Enter your name: ")
+    house = input("Enter your house : ")
+    data = Student(name,house)
+    return data 
+if __name__== "__main__":
+    main()

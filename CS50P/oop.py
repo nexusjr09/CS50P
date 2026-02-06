fd4cbb69b7da9ed1d9fde1383a2101de557ddfe8
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-06 17:17:24 +0545

    Tuples in Python

--- CS50P/oop.py
+++ CS50P/oop.py
@@ -1,14 +1,13 @@
 #OOP IN PYTHON
 
 def main():
-    name = get_name()
-    house = get_house()
+    name,house = get_student()
+
     print(f"{name} is from {house}")
 
-def get_name():
-    return input("Enter your name: ")
-def get_house():
-    return input("Enter your house: ")
-
+def get_student():
+    name = input("Enter your name: ")
+    house = input("Enter your house: ")
+    return name,house
 if __name__ == "__main__":
     main()

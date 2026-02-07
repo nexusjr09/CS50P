6a65f1dabe740ecdc824d0760819b29556b216ca
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-07 22:46:37 +0545

    changes in OOP

--- CS50P/ground.py
+++ CS50P/ground.py
@@ -5,10 +5,3 @@ def main():
     if student["name"] == "Abhishek":
         student["house"] = "Jhapa"
         print(f"{student["name"]} is from {student["house"]}")
-
-def get_student():
-    name = input("Enter the name: ")
-    house = input("Enter the house: ")
-    return {"name":name,"house":house}
-if __name__ == "__main__":
-    main()

d796d8766ec21c447c6e50c156b9f5b541c1cdbf
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-08 08:01:22 +0545

    changes in OOP

--- CS50P/ground.py
+++ CS50P/ground.py
@@ -1,7 +1,1 @@
 #OOP IN PYTHON
-
-def main():
-    student = get_student()
-    if student["name"] == "Abhishek":
-        student["house"] = "Jhapa"
-        print(f"{student["name"]} is from {student["house"]}")

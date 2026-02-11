e1ed796ac12e1fccf58cfe93baecd301bfa2b414
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-11 13:35:28 +0545

    OOP in python

--- CS50P/oop.py
+++ CS50P/oop.py
@@ -7,8 +7,6 @@ def main():
         print(f"{student["name"]} is from {student["house"]}")
 
 def get_student():
-    name = input("Enter the name: ")
-    house = input("Enter the house: ")
-    return ({"name":name,"house":house})
+   
 if __name__ == "__main__":
     main()

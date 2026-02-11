fdfb75f91d86c646c062fb1cbab2782dd34834f6
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-11 13:42:13 +0545

    class in Object oriented programming

--- CS50P/oop.py
+++ CS50P/oop.py
@@ -1,12 +1,14 @@
-#OOP IN PYTHON
+class Student:
+    ...
 
 def main():
     student = get_student()
-    if student["name"] == "Abhishek":
-        student["house"] = "Jhapa"
-        print(f"{student["name"]} is from {student["house"]}")
+    print(f"{student.name} is from {student.house}")
 
 def get_student():
-   
+    student = Student()
+    student.name = input("Enter student name: ")
+    student.house = input("Enter student house: ")
+    return student    
 if __name__ == "__main__":
     main()

a945bf97a7748355949b6d7bb49e3f18ae678e91
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-13 10:11:40 +0545

    changes in OOP

--- CS50P/oop.py
+++ CS50P/oop.py
@@ -6,30 +6,13 @@ class Student:
             raise ValueError("Invalid House Name !")
         self.name = name
         self.house = house
-        self.zodiac = zodiac
-    def __str__(self):
-        return f"{self.name} is from {self.house}"
-    def charm(self):
-        match self.zodiac:
-            case "Piecies":
-                return "🏹"
-            case "Libra":
-                return "🧭"
-            case "Scorpion":
-                return "🧪"
-            case _:
-                return "👺"
-
 def main():
-    student = get_student()
-    print("Zodiac sign !!@!")
-    print(student.charm())
+    ...
 
 def get_student():
     name = input("Enter the name: ")
     house = input("Enter the house: ")
-    zodiac = input("Enter your zodiac sign: ")
-    return Student(house,name,zodiac)
+    return Student(house,name)
 if __name__ == "__main__":
     main()
 

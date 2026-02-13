7021e3b14a117931cd1743a5ad5ec755e3f965f2
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-13 10:08:13 +0545

    changes in OOP

--- CS50P/oop.py
+++ CS50P/oop.py
@@ -1,17 +1,29 @@
 class Student:
-    def __init__(self,house,name):
+    def __init__(self,house,name,zodiac):
         if not name:#equals to if name ==""": 
             raise ValueError("Missing Name !")
         if house not in ["Kathmandu","Lalitpur","Bhaktapur"]:
             raise ValueError("Invalid House Name !")
         self.name = name
         self.house = house
+        self.zodiac = zodiac
     def __str__(self):
         return f"{self.name} is from {self.house}"
+    def charm(self):
+        match self.zodiac:
+            case "Piecies":
+                return "🏹"
+            case "Libra":
+                return "🧭"
+            case "Scorpion":
+                return "🧪"
+            case _:
+                return "👺"
 
 def main():
     student = get_student()
-    print(student)
+    print("Zodiac sign !!@!")
+    print(student.charm())
 
 def get_student():
     name = input("Enter the name: ")

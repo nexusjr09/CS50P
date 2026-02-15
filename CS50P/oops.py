20162570e8611c79b8280e09e7bba50a6ed486f4
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-15 10:05:49 +0545

    class and object in OOP

--- CS50P/oops.py
+++ CS50P/oops.py
@@ -1,1 +1,17 @@
-#FROM APNA COLLEGE 
+# class Student:
+#     name = "Bigyan"
+
+# s1 = Student()
+# print(s1.name)
+
+# s2 = Student()
+# print(s2.name)
+
+class Car:
+    color = "Blue"
+    model = "BMW"
+
+
+c1 = Car()
+print(c1.color)
+print(c1.model)

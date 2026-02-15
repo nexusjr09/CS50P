6e7e9a9bb6d36201ce4a1ee2fcc32dc1a86c9cc9
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-15 10:50:13 +0545

    self i OOPs

--- CS50P/oops.py
+++ CS50P/oops.py
@@ -1,16 +1,24 @@
 class Student:
-    def __init__(self,firstname,lastname,age):
+    #default constructors 
+    def __init__(self):
+        pass
+    #parameterized constructors
+    def __init__(self):
+        firstname = input("Enter name: ")
+        lastname = input("Enter the last name: ")
+        age = input("Enter the age of s1:  ")
+        
         self.name = firstname
         self.lname = lastname 
         self.age = age 
         print("All data created !")
 
-s1 = Student("Bigyan","Baral","18")
+s1 = Student()
 print(s1.name)
 print(s1.lname)
 print(s1.age)
 
-s2 = Student("Amrit","Subba","19")
+s2 = Student()
 print(s2.name)
 print(s2.lname)
-print(s1.age)
+print(s2.age)

61b914e57644dbe95e8c9ceaaf8d7c9d9a10d7fd
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-16 13:47:05 +0545

    seasons . py in cs50

--- CS50P/seasons.py
+++ CS50P/seasons.py
@@ -2,29 +2,24 @@ from datetime import date
 import sys
 import inflect
 
-
-
 def main():
-   data =  input("Enter your birthdate: ")
-   conversion(data)
-def conversion(x):
+    data = input("Birth Date: ")
+    print(convert(data))
+
+def convert(s):
     try:
-        birth = date.fromisoformat(x)
+        birth = date.fromisoformat(s)
     except ValueError:
         sys.exit("Invalid date")
-        
+
     today = date.today()
-    if birth > today:
-        sys.exit("Birth date cannot be in the future !")
-    difference = today - birth 
-    days = difference.days
-    totalhour = days * 24
-    totalminutes = totalhour * 60 
+    
+    difference = today - birth
+    minutes = difference.days * 24 * 60
+    
     p = inflect.engine()
-    words = p.number_to_words(totalminutes,andword="")
-    print(f"You have existed for total {words} Minutes in this earth !")
-
-
+    words = p.number_to_words(minutes, andword="")
+    return f"{words.capitalize()} minutes"
 
 if __name__ == "__main__":
     main()
--- CS50P/test_seasons.py
+++ CS50P/test_seasons.py
@@ -1,7 +1,14 @@
 import pytest
-from seasons import conversion
-def test_format():
-        
-        assert conversion("January 1 , 1992") == False
-        assert conversion("01-07-2000") == False
+from seasons import convert
 
+def test_valid_dates():
+    assert convert("2025-02-16") == "Five hundred twenty-five thousand, six hundred minutes"
+    assert convert("2024-02-16") == "One million, fifty-one thousand, two hundred minutes"
+
+def test_invalid_formats():
+    with pytest.raises(SystemExit):
+        convert("January 1, 1992")
+    with pytest.raises(SystemExit):
+        convert("1992/01/01")
+    with pytest.raises(SystemExit):
+        convert("01-01-1992")

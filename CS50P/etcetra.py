d38bd614cc6b4dae8188ec7bfdd2ff27dfa98043
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-20 08:16:11 +0545

    constants

--- CS50P/etcetra.py
+++ CS50P/etcetra.py
@@ -1,19 +1,19 @@
-balance = 0 
+# balance = 0 
 
-def main():
-    global balance 
-    print("Old Value for Balance: ", balance)
-    deposit(5000)
-    withdraw(1000)
+# def main():
+#     global balance 
+#     print("Old Value for Balance: ", balance)
+#     deposit(5000)
+#     withdraw(1000)
 
-def deposit(v):
-    global balance 
-    balance += v
-    print("Now the value is ",balance)
+# def deposit(v):
+#     global balance 
+#     balance += v
+#     print("Now the value is ",balance)
 
-def withdraw(n):
-    global balance 
-    balance -= n
-    print("Now the balance is",balance)
+# def withdraw(n):
+#     global balance 
+#     balance -= n
+#     print("Now the balance is",balance)
 
-main()
+# main()

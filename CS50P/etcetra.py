e991733f058cac00419591b861921c5b4e9b6ac9
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-20 08:00:24 +0545

    Global Variable in Python

---
+++ CS50P/etcetra.py
@@ -0,0 +1,19 @@
+balance = 0 
+
+def main():
+    global balance 
+    print("Old Value for Balance: ", balance)
+    deposit(5000)
+    withdraw(1000)
+
+def deposit(v):
+    global balance 
+    balance += v
+    print("Now the value is ",balance)
+
+def withdraw(n):
+    global balance 
+    balance -= n
+    print("Now the balance is",balance)
+
+main()

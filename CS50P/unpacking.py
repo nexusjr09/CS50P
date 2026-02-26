49aea832fb12c8f1ca08c68e71ba0c35cb44a42e
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-26 09:51:21 +0545

    agrs and kwarfs

--- CS50P/unpacking.py
+++ CS50P/unpacking.py
@@ -1,5 +1,12 @@
-#unpacking in python
-def total(galleons,sickles,knuts):
-    return (galleons * 17 + sickles) * 29 + knuts
-coins = [100,50,25]
-print(total(*coins)) # star (*) unpacks the list and sends three individual values . 
+# #unpacking in python
+# def total(galleons,sickles,knuts):
+#     return (galleons * 17 + sickles) * 29 + knuts
+# coins = {"galleons":100, "sickles": 50 , "knuts":25}
+# #print(total(*coins)) # star (*) unpacks the list and sends three individual values [FOR THE LIST].
+# # FOR THE DICTIONARIES YOU use two stars (**coins) 
+# #print(total(galleons = 100,sickles = 50 , knuts= 25))
+# print(total(**coins))  
+
+def f(*args,**kwargs):
+             print("Positional",args)
+f(100,50,25)

f5382df72f74da9f6a94b74ca727fbc21318e40f
Vigyan_X1 <134819649+nexusjr09@users.noreply.github.com>
2026-02-26 09:13:15 +0545

    unpacking in python

--- CS50P/unpacking.py
+++ CS50P/unpacking.py
@@ -2,4 +2,4 @@
 def total(galleons,sickles,knuts):
     return (galleons * 17 + sickles) * 29 + knuts
 coins = [100,50,25]
-print(total(coins[0],coins[1],coins[2]))
+print(total(*coins)) # star (*) unpacks the list and sends three individual values . 
